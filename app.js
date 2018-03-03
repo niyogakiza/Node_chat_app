9fc42759f9e979fe39bf1d57752c8281c7dfac8e
Aimable <niyoai@yahoo.fr>
2018-03-03 18:23:20 +0000

    Greet new User

--- app.js
+++ app.js
@@ -25,14 +25,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 io.on('connection', (socket) =>{
     console.log('New user connected');
 
+    //socket.emit from Admin text Welcome to the chat app
+    socket.emit('newMessage',{
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    // socket.broadcast.emit from Admin text new user joined
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
 
     socket.on('createMessage', (message) =>{
         console.log('createMessage', message);
+
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        })
+        });
+
+        // socket.broadcast.emit('newMessage', {
+        //         from: message.from,
+        //         text: message.text,
+        //         createdAt: new Date().getTime()
+        //     });
     });
 
     socket.on('disconnected', () =>{
