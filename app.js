acfa96bef15df6501d407e712e7e498d78cc5ff6
Aimable <niyoai@yahoo.fr>
2018-03-03 17:30:11 +0000

    Add connect and disconnect events

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
-
+const http = require('http');
+const path = require("path");
+const socketIO = require('socket.io');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
-const path = require("path");
+
 
 const app = express();
 const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socketIO(server);
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -14,12 +21,23 @@ app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// listen to connection that comes in
+io.on('connection', (socket) =>{
+    //console.log('New user connected');
+
+    socket.on('disconnected', () =>{
+        console.log('User was disconnected');
+    });
+});
+
+
+
 app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -35,7 +53,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(port, () =>{
+server.listen(port, () =>{
     console.log(`Server is up on port ${port}`);
 });
 
