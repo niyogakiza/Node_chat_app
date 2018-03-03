55f696bd624503b8953276013362cd92f788aaf1
Aimable <niyoai@yahoo.fr>
2018-03-03 19:58:11 +0000

    Add form for messages

--- server/app.js
+++ server/app.js
@@ -24,10 +24,11 @@ io.on('connection', (socket) =>{
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
 
-    socket.on('createMessage', (message) =>{
+    socket.on('createMessage', (message, callback) =>{
         console.log('createMessage', message);
 
         io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is from the server.');
 
         // socket.broadcast.emit('newMessage', {
         //         from: message.from,
@@ -63,5 +64,3 @@ server.listen(port, () =>{
     console.log(`Server is up on port ${port}`);
 });
 
-
-module.exports = app;
