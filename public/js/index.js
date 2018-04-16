f47d35402dca8727fa87cf90eec87f736552e3d3
Aimable <niyoai@yahoo.fr>
2018-04-16 18:27:55 +0100

    scrooll messages

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,27 @@
 
 const socket = io();
 
+function scrollToBottom (){
+
+    //Selectors
+    let messages = $('#messages');
+    let newMessage = messages.children('li:last-child');
+
+    //Heights
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+
+}
+
+
 socket.on('connect', function() {
     console.log('Connected to server');
 
@@ -23,6 +44,7 @@ socket.on('newMessage', function (message) {
 
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message){
@@ -36,7 +58,8 @@ socket.on('newLocationMessage', function(message){
         createdAt: formattedTime
     });
 
-     $('#messages').append(html)
+     $('#messages').append(html);
+     scrollToBottom();
 });
 
 
